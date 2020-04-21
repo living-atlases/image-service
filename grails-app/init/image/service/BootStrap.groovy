febb08e14c44fbdcd126a96b09f79fb579cbd7ff
vjrj <vjrj@ourproject.org>
2020-04-21 20:34:55 +0200

    Added ala-i18n support and crowdin configuration

--- grails-app/init/image/service/BootStrap.groovy
+++ grails-app/init/image/service/BootStrap.groovy
@@ -2,11 +2,19 @@ package image.service
 
 class BootStrap {
 
+    def messageSource
+
     def elasticSearchService
 
 
     def init = { servletContext ->
 
+        messageSource.setBasenames(
+                "file:///var/opt/atlas/i18n/image-service/messages",
+                "file:///opt/atlas/i18n/image-service/messages",
+                "WEB-INF/grails-app/i18n/messages",
+                "classpath:messages"
+        )
 
         elasticSearchService.initialize()
 
